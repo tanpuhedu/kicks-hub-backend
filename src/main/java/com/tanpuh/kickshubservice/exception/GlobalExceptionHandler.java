ee1d388018cbbb3795174c513cc26686ac700e3c
tanpuhedu <3122411191@sv.sgu.edu.vn>
2025-09-06 22:09:56 +0700

    refactor validation exception handler

--- src/main/java/com/tanpuh/kickshubservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tanpuh/kickshubservice/exception/GlobalExceptionHandler.java
@@ -1,15 +1,22 @@
 package com.tanpuh.kickshubservice.exception;
 
 import com.tanpuh.kickshubservice.dto.response.ApiResponse;
+import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    private static final Set<String> IGNORED_ATTRIBUTES = Set.of("message", "groups", "payload");
+
     // xử lí những lỗi chưa xác định được
     @ExceptionHandler(value = Exception.class)
     ResponseEntity<ApiResponse> handleRuntimeException(Exception e) {
@@ -38,15 +45,43 @@ public class GlobalExceptionHandler {
         String enumKey = exception.getFieldError().getDefaultMessage();
 
         ErrorCode errorCode = ErrorCode.KEY_INVALID;
+        Map<String, Object> attributes = null;
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+
+            var constraintViolation = exception.getBindingResult()
+                    .getAllErrors()
+                    .get(0)
+                    .unwrap(ConstraintViolation.class);
+            log.info(constraintViolation.toString());
+
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+            log.info(attributes.toString());
+
         } catch (IllegalArgumentException ignored) { }
 
-        ApiResponse apiResponse = ApiResponse.builder()
-                .status(errorCode.getCode())
-                .message(errorCode.getMessage())
-                .build();
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setStatus(errorCode.getCode());
+        apiResponse.setMessage(
+                Objects.nonNull(attributes)
+                        ? mapAttribute(errorCode.getMessage(), attributes)
+                        : errorCode.getMessage()
+        );
 
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String resolvedMessage = message;
+        for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+            String key = entry.getKey();
+
+            if (IGNORED_ATTRIBUTES.contains(key))
+                continue;
+
+            String value = String.valueOf(entry.getValue());
+            resolvedMessage = resolvedMessage.replace("{" + key + "}", value);
+        }
+        return resolvedMessage;
     }
 }
